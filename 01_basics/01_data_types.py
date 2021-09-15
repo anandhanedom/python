29fada8a9b81c9767c7c2c3fe92d44af89a9a16b
Anandhan <anandhanedom@gmail.com>
2021-09-15 23:53:14 +0530

    feat: string operations

--- 01_basics/01_data_types.py
+++ 01_basics/01_data_types.py
@@ -19,8 +19,22 @@ print(type(2 / 4))
 print(5 // 4)  # gets rounded to integer
 
 # strings
-long_string = """
+long_string = """ 
 wow
 wow
 wow
 """
+
+print(long_string)
+
+
+name = "john doe"
+age = 55
+print(f"hi {name}. you are {age} years old")  # like template strings in js
+
+message = "01234567"
+print(message[0:2])  # [start:stop] - stop is also excluded
+print(message[0:8:2])  # [start:stop:stepover]
+print(message[1:])  # start from index 1
+print(message[-1])  # last index
+print(message[::-1])  # string reversal
