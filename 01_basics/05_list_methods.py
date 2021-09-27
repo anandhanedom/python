07d9059363b95ee7f4f1425b57cfcbe0ee439756
Anandhan <anandhanedom@gmail.com>
2021-09-27 12:00:48 +0530

    feat: small backup

--- 01_basics/05_list_methods.py
+++ 01_basics/05_list_methods.py
@@ -19,3 +19,33 @@ print(basket)
 
 basket.remove("new-item")
 print(basket)
+
+
+# lookup
+new_basket = [
+    "e",
+    "a",
+    "b",
+    "c",
+    "d",
+]
+print(new_basket.index("a"))
+# print(new_basket.index("d",0,3)) // stops right before d
+
+
+print("z" in new_basket)  # exists?
+print(new_basket.count("d"))  # how many times?
+
+# new_basket.sort() - same array modification
+print(sorted(new_basket))  # returns new array
+
+
+latest = new_basket.copy()
+
+# new_basket.reverse()
+
+print(latest)
+
+
+### common list patters ###
+print(len(new_basket))
