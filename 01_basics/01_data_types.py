dac35d4856297096c1cf9802f7482bf03e9e5b49
Anandhan <anandhanedom@gmail.com>
2021-10-03 23:37:02 +0530

    fix: comment change

--- 01_basics/01_data_types.py
+++ 01_basics/01_data_types.py
@@ -38,7 +38,7 @@ age = 55
 print(f"hi {name}. you are {age} years old")  # like template strings in js
 
 message = "01234567"
-print(message[0:2])  # [start:stop] - stop is also excluded
+print(message[0:2])  # [start:stop] - stop is omitted
 print(message[0:8:2])  # [start:stop:stepover]
 print(message[1:])  # start from index 1
 print(message[-1])  # last index
