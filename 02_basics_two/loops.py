497b79f2618aaadd7a34692e211e6a4661fdef50
Anandhan <anandhanedom@gmail.com>
2021-11-12 17:53:27 +0530

    feat: for loops

---
+++ 02_basics_two/loops.py
@@ -0,0 +1,11 @@
+for alphabet in "hitman":
+    print(alphabet)
+
+
+for number in list(range(0, 10)):
+    print(number)
+
+
+for item in (1, 2, 3, 4):
+    for subItem in ("a", "b", "c"):
+        print(f"{item}{subItem}")
