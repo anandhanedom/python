6e50afc75328f1c7177ccf5f4985cb5ea39351aa
Anandhan <anandhanedom@gmail.com>
2021-11-15 13:38:32 +0530

    feat: simple functions

--- 02_basics_two/functions.py
+++ 02_basics_two/functions.py
@@ -1,3 +1,24 @@
 def say_hello():
     print("Hello world")
 
+
+def say_name(name="Darth Vader", icon="@"):
+    print(f"Helloo {name} {icon}")
+
+
+# postional arguments - GOOD
+say_name("John Doe", "#")
+
+# keyword arguments - BAD
+say_name(icon="#", name="Jane Doe")
+
+
+say_name()
+
+
+def add(a=0, b=0):
+    return a + b
+
+
+total = add(1, 2)
+print(total)
