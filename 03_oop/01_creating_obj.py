5ed62abd5de593b20c00f1c990afe1373544f16a
Anandhan <anandhanedom@gmail.com>
2021-11-15 20:54:45 +0530

    feat: static and class methods

--- 03_oop/01_creating_obj.py
+++ 03_oop/01_creating_obj.py
@@ -1,7 +1,7 @@
 class PlayerCharacter:
     membership = True  # class object attribute - static
 
-    def __init__(self, name, age):
+    def __init__(self, name="No Name", age=10):
         self.name = name  # attributes - dynamic
         self.age = age
 
@@ -10,10 +10,19 @@ class PlayerCharacter:
 
     def shout(self):
         print(f"My name is {self.name}")
-        if self.membership:  # if PlayerCharacter.membership also works
+        if self.membership:  # if PlayerCharacter.membership also works in this case
             print("I'm a member")
         else:
             print("I'm not a member")
+
+    @classmethod
+    def add_things(cls, num1, num2):
+        return cls("Jane Doe", num1 + num2)  # instantiation
+        # return num1 + num2
+
+    @staticmethod
+    def add_things2(num1, num2):
+        return num1 + num2
 
 
 player1 = PlayerCharacter("John Doe", 20)
@@ -26,3 +35,8 @@ print(player1.age)
 
 player1.run(10)
 player1.shout()
+
+print(player1.add_things(1, 2))  # on instance
+
+player_2 = PlayerCharacter.add_things(10, 2)  # on class
+print(player_2.age)
