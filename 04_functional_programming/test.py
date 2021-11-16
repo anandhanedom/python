d6fe427e233e6bd8ea9a5eacfa4e45f5da0b26f0
Anandhan <anandhanedom@gmail.com>
2021-11-16 22:44:38 +0530

    feat: test

--- 04_functional_programming/test.py
+++ 04_functional_programming/test.py
@@ -7,3 +7,8 @@ print(list(map(lambda item: item ** 2, my_list)))
 a = [(0, 2), (4, 3), (10, -1), (9, 9)]
 a.sort(key=lambda item: item[1])
 print(a)
+
+
+some_list = ["a", "b", "c", "b", "b", "d", "m", "n", "n"]
+duplicates = set([character for character in some_list if some_list.count(character) > 1])
+print(duplicates)
