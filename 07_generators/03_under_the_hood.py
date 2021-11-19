4e6416f3cf135b575275e7dacb2064a193a18231
Anandhan <anandhanedom@gmail.com>
2021-11-19 15:36:51 +0530

    feat: gen under the hood

--- 07_generators/03_under_the_hood.py
+++ 07_generators/03_under_the_hood.py
@@ -9,3 +9,29 @@ def special_for_loop(iterable):
 
 
 special_for_loop([1, 2, 3, 4])
+
+# range implementation
+
+
+class MyGen:
+    current = 0
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if MyGen.current < self.last:
+            num = MyGen.current
+            MyGen.current += 1
+            return num
+        raise StopIteration
+
+
+range_10 = MyGen(0, 10)
+
+for i in range_10:
+    print(i)
