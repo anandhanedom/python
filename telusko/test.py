81980754c816eb42b17285c9554e35b27966307e
Anandhan <anandhanedom@gmail.com>
2021-11-20 12:40:49 +0530

    test.

--- telusko/test.py
+++ telusko/test.py
@@ -7,3 +7,25 @@ def get_factorial(number):
 
 five_factorial = get_factorial(5)
 print(five_factorial)
+
+
+# decoratorsss
+
+
+def smart_divider(func):
+    def wrapper(a, b):
+        if a < b:
+            a, b = b, a
+        func(b, a)
+
+    return wrapper
+
+
+@smart_divider
+def div(a, b):
+    print(a / b)
+
+
+div(2, 4)
+print("**")
+div(4, 2)
