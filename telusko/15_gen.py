3eb5f347ac475abe7f56b2adfaec5687817b4285
Anandhan <anandhanedom@gmail.com>
2021-11-27 18:02:58 +0530

    feat: generators

---
+++ telusko/15_gen.py
@@ -0,0 +1,21 @@
+def topTen():
+    # yield 10
+    # yield 9
+    # yield 8
+    # yield 7
+    n = 1
+
+    while n <= 10:
+        sq = n * n
+        yield sq
+        n += 1
+        
+
+values = topTen()
+
+print(values.__next__())
+print(values.__next__())
+
+
+for i in values:
+    print(i)
