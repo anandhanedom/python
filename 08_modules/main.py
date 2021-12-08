dee27852fbb45651f6d5b676e22dca5d4ab45ae9
Anandhan <anandhanedom@gmail.com>
2021-12-08 17:43:48 +0530

    feat: todays code

---
+++ 08_modules/main.py
@@ -0,0 +1,5 @@
+import utility  # cached in pycache
+import shopping.shopping_cart
+
+print(utility)
+print(shopping.shopping_cart.buy("apple"))
---
+++ 08_modules/shopping/shopping_cart.py
@@ -0,0 +1,8 @@
+# shopping is package
+# shopping_cart is a module
+
+
+def buy(item):
+    cart = []
+    cart.append(item)
+    return cart
