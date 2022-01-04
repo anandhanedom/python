fb39d3d95da36f26d6d033c1654a82580eaeccbc
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-01-04 23:47:28 +0530

    feat(beautiful-soup): write scraped data to files

--- 010_beautiful_soup/scrape_website.py
+++ 010_beautiful_soup/scrape_website.py
@@ -1,37 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-html_text = requests.get(
-    "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation="
-).text
-
-# print(html_text)
-
-soup = BeautifulSoup(html_text, "lxml")
-
-# job = soup.find("li", class_="clearfix job-bx wht-shd-bx")
-# company_name = job.find("h3", class_="joblist-comp-name").text.replace(" ", "")
-# skills = job.find("span", class_="srp-skills").text.replace(" ", "")
-# published_date = job.find("span", class_="sim-posted").span.text
-
-# print(published_date)
-
-# print(company_name)
-# print(skills)
+print("Enter some skill that you are not familiar with!")
+unfamiliar_skill = input(">")
+print(f"Filtering out {unfamiliar_skill}....")
 
 
-# print(f"Company name: {company_name}")
-# print(f"Required skills: {skills}")
+def fetch_jobs():
+    html_text = requests.get(
+        "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation="
+    ).text
+    soup = BeautifulSoup(html_text, "lxml")
+    jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
+
+    for index, job in enumerate(jobs):
+        published_date = job.find("span", class_="sim-posted").span.text
+        if "few" in published_date:
+            company_name = job.find("h3", class_="joblist-comp-name").text.replace(
+                " ", ""
+            )
+            required_skills = job.find("span", class_="srp-skills").text.replace(
+                " ", ""
+            )
+            more_info = job.header.h2.a["href"]
+
+            if unfamiliar_skill not in required_skills:
+
+                with open(f"{index}.txt", "w") as new_file:
+
+                    new_file.write(f"Company name: {company_name.strip()} \n")
+                    new_file.write(f"Required skills: {required_skills.strip()} \n")
+                    new_file.write(f"More info: {more_info} \n")
+                    new_file.write("")
+                print(f"File saved: {index}")
 
 
-jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
-
-for job in jobs:
-    published_date = job.find("span", class_="sim-posted").span.text
-    if "few" in published_date:
-        company_name = job.find("h3", class_="joblist-comp-name").text.replace(" ", "")
-        required_skills = job.find("span", class_="srp-skills").text.replace(" ", "")
-
-        print(f"Company name: {company_name.strip()}")
-        print(f"Required skills: {required_skills.strip()}")
-        print("")
+if __name__ == "__main__":
+    while True:
+        fetch_jobs()
+        waiting_time = 10
+        print(f"Waiting {waiting_time} minutes...")
+        time.sleep(waiting_time * 60)
