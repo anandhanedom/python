54e745561b929f203165e578d78684a398150df8
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-01-21 23:08:31 +0530

    feat: more regex exp

--- telusko/017_regex.py
+++ telusko/017_regex.py
@@ -19,3 +19,20 @@ print(match1)
 
 # instances
 print(re.findall("match", "test phrase match in middle"))
+
+# meta character syntax
+def multi_re_find(patterns, phrase):
+    for pattern in patterns:
+        print(f"Searching for pattern {pattern}")
+        print(re.findall(pattern, phrase))
+        print("\n")
+
+
+test_phrase = "sdsd..sssddd..sdddsddd...dsds...dsds...dssssss...sddddd"
+# test_patterns = ["sd*"]  # s followed by 0 or more d's
+# test_patterns = ["sd+"]  # s followed by 1 or more d's
+# test_patterns = ["sd?"]  # s followed by 0 or 1 d
+# test_patterns = ["sd{3}"]  # s followed by 3 d's
+test_patterns = ["sd{2,3}"]  # s followed by 2 or 3 d's
+
+multi_re_find(test_patterns, test_phrase)
