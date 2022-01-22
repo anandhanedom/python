dcf7f54d1d72f53752973937cf902c9cbc5d8c66
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-01-22 12:16:49 +0530

    feat: complete regex basics

--- telusko/017_regex.py
+++ telusko/017_regex.py
@@ -28,11 +28,28 @@ def multi_re_find(patterns, phrase):
         print("\n")
 
 
-test_phrase = "sdsd..sssddd..sdddsddd...dsds...dsds...dssssss...sddddd"
+test_phrase = "This is a string with numbers 12312 and a symbol #hashtag"
+
+# test_phrase = "This is a string! But has punctuation. How can we remove it?"
+# test_phrase = "sdsd..sssddd..sdddsddd...dsds...dsds...dssssss...sddddd"
+
 # test_patterns = ["sd*"]  # s followed by 0 or more d's
 # test_patterns = ["sd+"]  # s followed by 1 or more d's
 # test_patterns = ["sd?"]  # s followed by 0 or 1 d
 # test_patterns = ["sd{3}"]  # s followed by 3 d's
-test_patterns = ["sd{2,3}"]  # s followed by 2 or 3 d's
+# test_patterns = ["sd{2,3}"]  # s followed by 2 or 3 d's
+# test_patterns = ["s[sd]+"]  # s followed by 1 or more s's or d's
+
+# test_patterns = ["[^!.?]+"]  # excluding symbols
+# test_patterns = ["[a-z]+"]
+# test_patterns = ["[A-Z]+"]
+
+# test_patterns = [r"\d+"]
+# test_patterns = [r"\D+"]
+# test_patterns = [r"\s+"]
+# test_patterns = [r"\S+"]
+# test_patterns = [r"\w+"]
+test_patterns = [r"\W+"]
+
 
 multi_re_find(test_patterns, test_phrase)
